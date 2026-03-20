8d5cbf92b9275a652a24bb99a3daa329c6c5c2f8
HancelE1993 <hancelxyz@gmail.com>
2026-03-20 17:22:08 -0500

    El primero gana

--- Introduccion a Devops Taller 1/Billetera/Billetera.java
+++ Introduccion a Devops Taller 1/Billetera/Billetera.java
@@ -1,6 +1,6 @@
 public class Billetera {
     double balanceTotal;
-    String moneda=USD;
+    String moneda = USD;
     String titular;
     boolean cuentaActiva = true;
 
@@ -8,19 +8,17 @@ public class Billetera {
         this.titular = titular;
         this.saldo = saldoInicial;
     }
-    
+
     public void transferir(double monto) {
-<<<<<<< HEAD
+
         if (monto > balanceTotal) {
             System.out.println("Fondos insuficientes");
             return;
         }
+        System.out.println("Iniciando transaccion...");
 
-=======
-        System.out.println("Iniciando transaccion...")
->>>>>>> fa57878353f2f2893f42379371bdaf1c95f0bcf8
         balanceTotal = balanceTotal - monto;
-        Systemo.out.println("Transferencia exitosa de $: "+ monto);
+        Systemo.out.println("Transferencia exitosa de $: " + monto);
     }
 
     public void mostrarSaldo() {
