8b64e2e2952449139026b74f0058abc0ea2214ac
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-03-20 18:54:20 -0400

    Resuelvo conflicto

--- Introduccion a Devops Taller 1/Billetera/Billetera.java
+++ Introduccion a Devops Taller 1/Billetera/Billetera.java
@@ -1,28 +1,28 @@
 public class Billetera {
     double balanceTotal;
-    String moneda = USD;
+    String moneda = "USD";
     String titular;
     boolean cuentaActiva = true;
 
-  
     public Billetera(String titular, double saldoInicial) {
         this.titular = titular;
         this.balanceTotal = saldoInicial;
     }
 
     public void transferir(double monto) {
+        System.out.println("Iniciando transacción...");
+
         if (monto > balanceTotal) {
             System.out.println("Fondos insuficientes");
             return;
         }
-        System.out.println("Iniciando transaccion...");
 
-        System.out.println("Iniciando transaccion...");
-        balanceTotal = balanceTotal - monto;
-        Systemo.out.println("Transferencia exitosa de $: " + monto);
+        balanceTotal = balanceTotal - (monto * 1.05);
+
+        System.out.println("Transferencia exitosa de: $" + monto);
     }
 
     public void mostrarSaldo() {
         System.out.println("Saldo de " + titular + ": $" + balanceTotal);
     }
-}
+}   
