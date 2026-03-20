37a0c4e514e321a0b3c0f7c615f584fcf6701242
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-03-20 19:11:16 -0400

    Agrega Fase 3

--- Introduccion a Devops Taller 1/Billetera/Main.java
+++ Introduccion a Devops Taller 1/Billetera/Main.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Billetera miBilletera = new Billetera("Juan Perez", 100.0);
         Billetera miBilletera2 = new Billetera("Maria Lopez", 500.0);
 
-        System.out.println("===BIENVENIDO AL SISTEMA BANCARIO===");
+        System.out.println("=== BIENVENIDO AL SISTEMA BANCARIO ===");
 
         miBilletera2.mostrarSaldo();
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("¿Cuánto desea transferir? ");
+        double monto = sc.nextDouble();
+
         miBilletera.mostrarSaldo();
-        miBilletera.transferir(20.0);
+        miBilletera.transferir(monto);
         miBilletera.transferir(30.0);
         miBilletera.transferir(10.0);
         miBilletera.mostrarSaldo();
